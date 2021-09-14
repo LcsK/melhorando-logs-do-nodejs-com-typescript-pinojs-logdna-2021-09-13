3a9e7f94c9d2c2fff17af040cdc0dca29614fd0c
lcsk <lucasaferrlopes@gmail.com>
2021-09-14 20:32:25 -0300

    feat: add parseLoggerInputToPinoFormat
    - this function parse the LogData from the commom interface to the pino.js
    format

--- src/Logger/Logger.ts
+++ src/Logger/Logger.ts
@@ -3,11 +3,17 @@ import { Logger, LogData } from './types';
 
 const pinoLogger = pino();
 
+const parseLoggerInputToPinoFormat = <T> ({ message, error, ...data }: LogData<T>) => ({
+	msg: message,
+	err: error,
+	...data,
+})
+
 const AppLogger: Logger = {
-	debug: <T> (logData: LogData<T>) => pinoLogger.debug(logData),
-	info: <T> (logData: LogData<T>) => pinoLogger.info(logData),
-	warn: <T> (logData: LogData<T>) => pinoLogger.warn(logData),
-	error: <T> (logData: LogData<T>) => pinoLogger.error(logData),
+	debug: <T> (logData: LogData<T>) => pinoLogger.debug(parseLoggerInputToPinoFormat(logData)),
+	info: <T> (logData: LogData<T>) => pinoLogger.info(parseLoggerInputToPinoFormat(logData)),
+	warn: <T> (logData: LogData<T>) => pinoLogger.warn(parseLoggerInputToPinoFormat(logData)),
+	error: <T> (logData: LogData<T>) => pinoLogger.error(parseLoggerInputToPinoFormat(logData)),
 };
 
 export default (): Logger => AppLogger;
